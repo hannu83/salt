a9cb132c5469a4caf1b735ada59a064e24d9bcdc
hannu83 <hannu.kankkunen1@gmail.com>
2018-11-15 11:20:23 +0000

    Added live-folder and run.sh

---
+++ run.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# Copyright 2018 Tero Karvinen http://TeroKarvinen.com GPL 3
+
+echo "Live USB configuration Starting... hannukankkunen/liveusb"
+set -o verbose
+
+echo "Installing Salt and Git..."
+sudo apt-get update
+sudo apt-get -y install git salt-minion
+
+echo "Retrieving and Applying Settings..."
+echo 'master: 142.93.160.134'|sudo tee /etc/salt/minion
+git clone https://github.com/hannu83/salt/live
+cd live/
+./highstate.sh
+
+echo "Configuring Live Desktop"
+setxkbmap fi
+sudo timedatectl set-timezone Europe/Helsinki
+git config --global credential.helper "cache --timeout=3600"
+git config --global push.default simple
+# git config --global user.email "hannu.kankkunen1@gmail.com"
+# git config --global user.name "Hannu Kankkunen"
+
+echo "More tips on http://terokarvinen.com/tag/saltstack"
+echo "Done."

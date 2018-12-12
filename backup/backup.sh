0f2a800b06ea042f1f4f88d2859524e5e22d1340
hannu83 <hannu.kankkunen1@gmail.com>
2018-12-12 13:24:00 +0000

    Working backup.sh

--- backup/backup.sh
+++ backup/backup.sh
@@ -1,17 +1,15 @@
 #This is the backupfile.
 #!/bin/sh
-
 THESITE="html"
-THEDB="books"
-THEDBUSER="books"
-THEDBPW="password"
+THEDB="books" 
+THEDBUSER="books" 
+THEDBPW="password" 
 THEDATE=`date +%d%m%y%H%M`
 
 mysqldump -u $THEDBUSER -p${THEDBPW} $THEDB | gzip > /home/xubuntu/Public/dbbackup_${THEDB}_${THEDATE}.bak.gz
 
-tar czf /home/xubuntu/Public/sitebackup_${THESITE}_${THEDATE}.tar -C / var/www
+tar cvf /home/xubuntu/Public/sitebackup_${THESITE}_${THEDATE}.tar -C / var/www
 gzip /home/xubuntu/Public/sitebackup_${THESITE}_${THEDATE}.tar
 
 find /home/xubuntu/Public/site* -mtime +5 -exec rm {} \;
 find /home/xubuntu/Public/db* -mtime +5 -exec rm {} \;
-
